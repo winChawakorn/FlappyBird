731d4bf470a4e73900a14725dd9934ba8cfac53f
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2018-03-15 23:50:02 +0700

    add map

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './Gui/Welcome/Home';
 import './Gui/Welcome/Home.css';
+import Map from './Gui/Map/Map'
 
 class App extends Component {
   render() {
     return (
       <div>
-          <Home />
+          {/*<Home />*/}
+          <Map/>
       </div>
     );
   }
---
+++ src/Gui/Map/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Map.css'
+
+class Map extends React.Component {
+  render() {
+    return (<div className="map-background">
+            </div>)
+  }
+}
+
+export default Map
