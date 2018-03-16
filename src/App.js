b92b45520f5e46d714e16035a5c88605e3916fed
winChawakorn <chawakorn.su@ku.th>
2018-03-16 18:44:05 +0700

    link pages

--- src/App.js
+++ src/App.js
@@ -4,23 +4,40 @@ import './App.css';
 import Home from './Gui/Welcome/Home';
 import Menu from './Gui/Welcome/Menu';
 import Obstacle from './Gui/Map/Obstacle';
-import './Gui/Welcome/Home.css';
 import Game from './Controller/Game'
 import Bird from './Gui/Bird/Bird'
+import Signup from './Gui/Welcome/Signup'
 
 class App extends Component {
 
-  componentDidMount() {
+  constructor() {
+    super()
+    this.state = {
+      page: "home",
+      login: false
+    }
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(page, login) {
+    this.setState({
+      page: page,
+      login: login
+    });
   }
 
   refresh() {
     this.forceUpdate()
   }
+
   render() {
-    Game.refresh = () => {this.refresh()}
+
+    Game.refresh = () => { this.refresh() }
     return (
       <div>
-          
+        { this.state.page==="home" && <Home changePage={this.changePage} />}
+        { this.state.page==="menu" && <Menu changePage={this.changePage} login={this.state.login} /> }
+        { this.state.page==="signup" && <Signup changePage={this.changePage}/>}
       </div>
     );
   }
--- src/Gui/Welcome/Menu.js
+++ src/Gui/Welcome/Menu.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import './Menu.css';
 
 class Menu extends React.Component {
+    userinfo() {
+        if(this.props.login){
+            return (
+                <div style={{display: 'flex', width: '150px'}}>
+                    <div className="profile-pic">pic</div>
+                    <span style={{margin: 'auto'}}>username</span>
+                </div>
+            );
+        }
+        return <div>Guest</div>
+    }
+
     render() {
         return (
-            <div className="menu">
-                <h1>FLAPPY BIRD</h1>
-                <button>Start</button>
-                <button>Leaderboard</button>
-                <button>Option</button>
-                <button>Logout</button>
+            <div>
+                <div className="userinfo">
+                    {this.userinfo()}
+                </div>
+                <div className="menu">
+                    <h1>FLAPPY BIRD</h1>
+                    <button>Start</button>
+                    <button>Scoreboard</button>
+                    <button>Option</button>
+                    <button onClick={() => {this.props.changePage('home')}}>Logout</button>
+                </div>
             </div>
         );
     }
